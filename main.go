f00a94f9005547c600efd489fd8428f1500d0061
Mahyuddin Husairi <mahyuddin@gmail.com>
2016-03-30 19:45:01 +0800

    multiple link experiment
    add features to check link status using packet.Dial. Will figure out a
    better solution.

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
 	"encoding/json"
 	"flag"
 	"os"
+	"time"
 
 	"github.com/go-ndn/log"
 	"github.com/go-ndn/ndn"
+	"github.com/go-ndn/packet"
 )
 
 var (
-	flagConfig = flag.String("config", "bridge.json", "config path")
+	flagConfig = flag.String("config", "go-onlsr.json", "config path")
 	flagDebug  = flag.Bool("debug", false, "enable logging")
 )
 
@@ -46,24 +48,80 @@ func main() {
 	}
 	log.Println("key", key.Locator())
 
-	// local face
-	local, err := newFace(config.Local.Network, config.Local.Address, nil)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer local.Close()
-	// remote face
-	recv := make(chan *ndn.Interest)
-	remote, err := newFace(config.Remote.Network, config.Remote.Address, recv)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer remote.Close()
+	// experiment using dummy neighbour list
 
-	go local.advertise(remote)
+	// get neighbour list from config file
+	neighbourList := config.Remote
 
-	// create remote tunnel
-	for i := range recv {
-		local.ServeNDN(remote, i)
-	}
+	linkedNeighbours := make(map[string]Neighbour)
+
+    // dummy loop for temporary solution.
+	for neighbourList != nil {
+        
+        log.Println("Neighbour size :", len(linkedNeighbours))
+
+		// Check if linked neighbour is still available.
+        // Temporarily, we use packet.Dial to check remote node is running ndn forwareder.
+        // We will figure out a better approaches.
+		for address, neighbour := range linkedNeighbours {
+            checkLink, err := packet.Dial(neighbour.Network, address)
+            if err != nil {
+                delete(linkedNeighbours, address)
+                log.Println(err)
+            } else {
+                checkLink.Close()
+            }
+		}
+        
+		for _, neighbour := range neighbourList {
+			go func(network string, address string, cost uint64) {
+
+				// only add new neighbour as remote face if not linked
+                if _, linked := linkedNeighbours[address]; !linked {
+
+                    // create interest channel
+                    interestChan := make(chan *ndn.Interest)
+                    
+                    // remote face
+                    remote, err := newFace(network, address, cost, interestChan)
+                    if err != nil {
+                        delete(linkedNeighbours, address)
+                        log.Println(err)
+                    } else {               
+                        // local face
+                        local, err := newFace(config.Local.Network, config.Local.Address, 0, nil)
+                        if err != nil {
+                            log.Fatalln(err)
+                        }
+                        defer local.Close()
+                        
+                        defer remote.Close()
+                        
+                        // Register remote face as linked neighbour
+                        linkedNeighbours[address]  = Neighbour {
+                            
+                                Network : network, 
+                                Address : address,
+                                Cost : cost,
+                                LocalFace : local, 
+                                RemoteFace : remote,
+                        }    
+
+                        // advertise name prefix
+                        go local.advertise(remote)
+
+                        // create remote tunnel
+                        for interest := range interestChan {
+                            local.ServeNDN(remote, interest)
+                        }
+                    }
+                    
+                }
+			}(neighbour.Network, neighbour.Address, neighbour.Cost)
+		}
+
+		helloIntv := time.Duration(config.HelloInterval) * time.Second
+		time.Sleep(helloIntv)
+	} // while loop
+
 }
--- neighbour.go
+++ neighbour.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+//  "fmt"
 //	"time"
 
-	"github.com/go-ndn/log"
+//	"github.com/go-ndn/log"
 //	"github.com/go-ndn/mux"
-	"github.com/go-ndn/ndn"
+//	"github.com/go-ndn/ndn"
 //	"github.com/go-ndn/tlv"
 )
 
 type Neighbour struct {
 	Address string
-	Cost uint64
-	ndn.Face
-	log.Logger
+    Network string
+    Cost uint64
+	LocalFace *face
+    RemoteFace *face
 }
 
-func NewNeighbour()() {
-	
+type AvailableNeighbour struct {
+	Address string
+    Network string
+	MacAddress string
+    Cost uint64
+}
+
+func newNeighbour()() {
+
 }
